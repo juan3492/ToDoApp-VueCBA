d46baf056ec8d732365b594a7ffba0cc83f21e7d
juan3492 <juanpunky3492@gmail.com>
2020-04-05 18:23:17 -0300

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header class="container">
+      <span class="h3">TODO App</span>
+      <span>-</span>
+      <span class="nes-text is-error">Primera etapa</span>
+    </header>
+
+    <div class="container nes-container with-title task-form">
+      <p class="title">Escriba aqui su tarea</p>
+      <form v-on:submit.prevent="addTask">
+        <textarea v-model="currTask" class="nes-textarea"></textarea>
+        <button type="submit" class="nes-btn is-success">Agregar Tarea</button>
+      </form>
+      <p class="nes-text is-error mt-2" v-show="showError"> 
+        Alto ahi, rufian. Primero debes escribir la tarea.
+      </p>
+    </div>
+    <div>
+
+      <br>
+
+      <div class="container nes-container with-title task-list"
+          v-show="tasks.length > 0"  
+      >
+        <p class="title">Tareas</p>
+        <div class="task" v-for="(task, index) in tasks" :key="index">
+          <div class="nes-container is-rounded">
+            <div class="row">
+              <div class="col">
+                <p @click="task.edit=true" v-if="!task.edit">{{task.task}}</p>
+                <textarea class="nes-textarea" v-else v-model="task.task" cols="40" rows="10"></textarea>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-11 ">
+                <label v-if="!task.edit">
+                  <input type="checkbox" class="nes-checkbox" v-model="task.complete"/>
+                  <span>Completada?</span>
+                </label>
+                <button class="nes-btn is-warning" v-else @click="task.edit=false">Terminar de editar</button>
+              </div>
+              <div class="col">
+                <button class="nes-btn is-error" @click="deleteTask(index)">x</button>
+              </div>
+            </div>
+          </div>
+          <br>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'nes.css/css/nes.min.css';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      currTask: '',
+      tasks: [],
+      error: '',
+      showError: false,
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.currTask !== '') {
+        this.tasks.push({task: this.currTask, complete: false, edit: false});
+        this.currTask = '';
+        this.showError = false
+      } else {
+        this.showError = true;
+      }
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
+    },
+    editTask(){
+      console.log("holaa")
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+header {
+  margin-bottom: 1%;
 }
+
+
+.nes-container{
+  word-wrap: break-word;
+}
+
+
+
+
 </style>
